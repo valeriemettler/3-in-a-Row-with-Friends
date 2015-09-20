36d715d47c2f0131f7c0c12e2355b2b4515eb2a4
Valerie Mettler <valeriemettler@gmail.com>
2015-09-19 17:59:05 -0700

    refactored code
    generated tictactoe grid with JavaScript instead of html

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 var counter = 0;
 
+var displayBoard = function () {
+    var x = "";
+    x = x + '<table width="300" height="300">';
+    var n = 2;
+    for (var i = 0; i <= n; i++){
+          x = x + ('<tr>');
+       for (var j = 0; j <= n; j++) {
+            x = x + '<td><div class="field"></div></td>';
+           }
+    }
+    x = x + '</tr></table>';
+    $('#x').html(x);
+}
+displayBoard();
+
 $('div').on('click', function(e) {
     var that = this;
     if ($(that).html() !== "") {
