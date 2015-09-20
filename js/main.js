bfe5ee01096ee0578cde70c41122bd363e337c0f
Valerie Mettler <valeriemettler@gmail.com>
2015-09-19 18:03:32 -0700

    cleaned up code format

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 var counter = 0;
 
-var displayBoard = function () {
+var displayBoard = function() {
     var x = "";
     x = x + '<table width="300" height="300">';
     var n = 2;
-    for (var i = 0; i <= n; i++){
-          x = x + ('<tr>');
-       for (var j = 0; j <= n; j++) {
+    for (var i = 0; i <= n; i++) {
+        x = x + ('<tr>');
+        for (var j = 0; j <= n; j++) {
             x = x + '<td><div class="field"></div></td>';
-           }
+        }
     }
     x = x + '</tr></table>';
     $('#x').html(x);
@@ -17,26 +17,26 @@ var displayBoard = function () {
 
 
 $(document).ready(function() {
-displayBoard();
+    displayBoard();
 
-$('div').on('click', function(e) {
-    var that = this;
-    if ($(that).html() !== "") {
-        return;
-    }
-    var x;
+    $('div').on('click', function(e) {
+        var that = this;
+        if ($(that).html() !== "") {
+            return;
+        }
+        var x;
 
-    var isOdd = function(counter) {
-        x = (counter % 2);
-        return x;
-    };
-    isOdd(counter);
+        var isOdd = function(counter) {
+            x = (counter % 2);
+            return x;
+        };
+        isOdd(counter);
 
-    if (x === 1) {
-        $(that).html("X");
-    } else {
-        $(that).html("0");
-    }
-    counter = counter + 1;
-})
+        if (x === 1) {
+            $(that).html("X");
+        } else {
+            $(that).html("0");
+        }
+        counter = counter + 1;
+    })
 });
