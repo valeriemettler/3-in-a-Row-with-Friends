c14b5342489335e7eac371a33303b6e32a8a051c
Valerie Mettler <valeriemettler@gmail.com>
2015-09-20 10:01:27 -0700

    Display data from Firebase to browser

--- js/main.js
+++ js/main.js
@@ -11,17 +11,54 @@ var displayBoard = function() {
     for (var i = 0; i <= n; i++) {
         x = x + ('<tr>');
         for (var j = 0; j <= n; j++) {
-            x = x + '<td><div class="field" box-id="q' + i + '' + j + '"></div></td>';
+            x = x + '<td><div class="field" id="q' + i + '' + j + '"></div></td>';
         }
     }
     x = x + '</tr></table>';
     $('#x').html(x);
 }
 
+fb.on('child_added', function(item) {
+    var token = item.val();
+    addToken(token.boxIndex, token.letter);
+    var r = item.val();
+    //console.log(r); // prints the dictionary objects of boxIndex:letter from firebase
+    var boxIndex = item.val()['boxIndex'];
+    //console.log("boxIndex", boxIndex);
+    var letter = item.val()['letter'];
+    //console.log("letter", letter);
+    //$(boxIndex).html(letter);
+    //$("#q01").html(letter);
 
+});
+
+var addToken = function(boxIndex, letter) {
+    $("#" + boxIndex).html(letter);
+    console.log("#" + boxIndex);
+    console.log('#q01');
+   // $(boxIndex).html(letter);
+};
+
+var onComplete = function(error) {
+    if (error) {
+        console.log('Firebase Synchronization failed');
+    } else {
+        console.log('Firebase Synchronization succeeded');
+    }
+};
+
+var deleteData = function() {
+    $('button').on('click', function() {
+        fb.remove(onComplete);
+        location.reload();
+    })
+};
 
 $(document).ready(function() {
     displayBoard();
+    deleteData();
+    //var k = addToken();
+    //console.log(k);
 
     $('div').on('click', function(e) {
         var that = this;
@@ -44,8 +81,8 @@ $(document).ready(function() {
         }
         counter = counter + 1;
 
-        var boxIndex = $(that).attr('box-id');
-        console.log($(that).attr('box-id'));
+        var boxIndex = $(that).attr('id');
+        //console.log($(that).attr('id'));
         var letter = $(that).html();
 
         fb.push({
