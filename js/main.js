e023de09a5e5ef9c7a2341978c99af463e4e06bc
Valerie Mettler <valeriemettler@gmail.com>
2015-09-21 09:56:07 -0700

    cleaned up code format

--- js/main.js
+++ js/main.js
@@ -3,19 +3,22 @@ var item;
 var counter = 0;
 var childAdded;
 
+var makeBoard = function() {
+    var boardNum = parseInt(Math.random() * (1000 - 1) + 1);
+    $('#boardInput').val(boardNum);
+    fb = new Firebase('https://valtictactoegame.firebaseio.com/' + boardNum);
+    displayBoard();
+    startGame();
+    reset();
+};
 
 var join = function() {
-    $('#buttonSet').on('click', function() { //gets called once at the beginning to join game
-        // clearBoard();
-        // $('#boardInput').keypress(function (e) {
-        //   console.log("I'm here!!!");
-        //   if (e.keyCode == 13) {
+    $('#buttonSet').on('click', function() {
         var boardInput = $('#joinInput').val();
         fb = new Firebase('https://valtictactoegame.firebaseio.com/' + boardInput);
         displayBoard();
         startGame();
         reset();
-        //console.log("join game");
     })
 };
 
@@ -33,38 +36,23 @@ var displayBoard = function() {
     $('#x').html(x);
 }
 
-var startGame = function() { //gets called on load and with each child added
+var startGame = function() {
     fb.off('child_added');
     fb.on('child_added', function(item) {
         var token = item.val();
         console.log(token);
-        addToken(token.boxIndex, token.letter); //called twice???
+        addToken(token.boxIndex, token.letter);
         var boxIndex = item.val()['boxIndex'];
         var letter = item.val()['letter'];
-        counter = counter + 1; //called twice???
-        console.log("start", counter);
+        counter = counter + 1;
     });
 };
 
-var addToken = function(boxIndex, letter) { //called twice???
-    console.log("addtoken");
+var addToken = function(boxIndex, letter) {
     $("#" + boxIndex).html(letter);
 };
 
-var onComplete = function(error) {
-    if (error) {
-        console.log('Firebase Synchronization failed');
-    } else {
-        console.log('Firebase Synchronization succeeded');
-    }
-};
 
-var deleteData = function() {
-    $('#deleteData').on('click', function() {
-        fb.remove(onComplete);
-        location.reload();
-    })
-};
 
 var reset = function() {
     $('.submit').on('click', '.field', function(e) {
@@ -99,27 +87,31 @@ var reset = function() {
     })
 };
 
+var newGame = function() {
+    $('#buttonNew').on('click', function() {
+        makeBoard();
+    })
+};
+
+var onComplete = function(error) {
+    if (error) {
+        console.log('Firebase Synchronization failed');
+    } else {
+        console.log('Firebase Synchronization succeeded');
+    }
+};
+
+var deleteData = function() {
+    $('#deleteData').on('click', function() {
+        fb.remove(onComplete);
+        location.reload();
+    })
+};
+
 $(document).ready(function() {
     displayBoard();
     deleteData();
     join();
-
-    var makeBoard = function() { //this gets run on loading the page
-        // $('#buttonNew').on('click', function() {
-        //var fb = new Firebase('http://test2player.firebaseio.com/');
-        var boardNum = parseInt(Math.random() * (1000 - 1) + 1);
-        //console.log(boardNum);
-        // $('#boardNum').html(boardNum);
-        $('#boardInput').val(boardNum);
-        fb = new Firebase('https://valtictactoegame.firebaseio.com/' + boardNum);
-        //console.log('http://test2player.firebaseio.com/' + boardNum);
-        displayBoard();
-        startGame();
-        reset();
-        //console.log("makeboard");
-
-        // });
-    }
     makeBoard();
-
+    newGame();
 });
