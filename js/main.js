72f65a1e81ff11d04817a48e2beea1ba233f5132
Valerie Mettler <valeriemettler@gmail.com>
2015-09-29 00:46:48 -0700

    restyled input fields and buttons

--- js/main.js
+++ js/main.js
@@ -146,10 +146,34 @@ var deleteData = function() {
     })
 };
 
+
+
+
+
 $(document).ready(function() {
     displayBoard();
     deleteData();
     joinGame();
     loadBoard();
     newGame();
+
+/////////
+
+ var myDataRef = new Firebase('https://test2player.firebaseio.com/');
+      $('#messageInput').keypress(function (e) {
+        if (e.keyCode == 13) {
+          var name = $('#nameInput').val();
+          var text = $('#messageInput').val();
+          myDataRef.push({name: name, text: text});
+          $('#messageInput').val('');
+        }
+      });
+      myDataRef.on('child_added', function(snapshot) {
+        var message = snapshot.val();
+        displayChatMessage(message.name, message.text);
+      });
+      function displayChatMessage(name, text) {
+        $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+        $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+      };
 });
